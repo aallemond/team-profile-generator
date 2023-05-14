fca68251dbba3b3399354805b00d06acca1710e0
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-05-14 18:42:56 -0500

    finished writing tests fixed bug where officeNum was returning undefined

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,10 +1,35 @@
-const Employee = require('./lib/Employee');
+const Employee = require('../lib/Employee');
 
 test('creates an employee object', () =>{
-    const employee = new Employee('Andrew', '30', 'allemondandrew@gmail.com');
+    const employee = new Employee('Andrew', 3000, 'allemondandrew@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets role of employee', () => {
+    const employee = new Employee ('Andrew', 3000, 'allemondandrew@gmail.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+});
+
+test('gets name of employee using getName()', () => {
+    const employee = new Employee ('Andrew', 3000, 'allemondandrew@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets get employee id', () => {
+    const employee = new Employee ('Andrew', 3000, 'allemondandrew@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets employee email using getEmail()', () => {
+    const employee = new Employee ('Andrew', 3000, 'allemondandrew@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
 
